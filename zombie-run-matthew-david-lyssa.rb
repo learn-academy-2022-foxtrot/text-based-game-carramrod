345dd2ae32ba115cb7a16fd636e83acd4c9a69bb
rhodesmt <mrhodes415@gmail.com>
2022-09-29 15:06:23 -0700

    Updated Game Logic and Restart Function

--- zombie-run-matthew-david-lyssa.rb
+++ zombie-run-matthew-david-lyssa.rb
@@ -1,4 +1,4 @@
-
+  def game_start
   puts 'You wake up to a strange noise. What do you do?'
     puts 'Search the house? Go outside? Do nothing?'
     answer1 = gets.chomp.downcase
@@ -10,7 +10,7 @@
       
       elsif (answer1.include?('outside'))
         p 'You are killed by a swarm of zombies'
-        exit
+        game_start()
       else (answer1.include?('nothing'))
         p 'Zombies break into your house and eat the flesh off of your bones... you die'
         exit
@@ -77,13 +77,58 @@ def choice4 (answer4)
   if (answer4.include?('car'))
    p 'You begin driving through the city. The car runs out of gas. The noise of the car attracted a zombie mob. Your weapons are no match.... you die'
    exit
-    elsif (answer4.include?('bike'))
-     p 'You start riding the bike. It has a squeeky chain. To avoid attracting zombies, you stop at the hardware store to find WD-40. When you enter the store, it is full of zombies. You get cornered... you die'
-     exit
-      elsif (answer4.include?('walk'))
-        p 'You begin your trek across the city...'
-  end
-end
+  elsif (answer4.include?('walk'))
+    p 'You begin your trek across the city...'
+  elsif (answer4.include?('bike'))
+    p 'You start riding the bike. It has a squeeky chain. To avoid attracting zombies, you stop at the hardware store to find WD-40. While searching the store, you hear a noise coming from the back room. Do you investigate?'
+    answer4_1 = gets.chomp.downcase
+        if (answer4_1 == 'yes')
+          p 'You search the room and find another survivor huddled in the corner. He tells you his name is Glenn, and he invites you to join him on his way to the Sanctuary. Do you accept?'
+          answer4_1_1 = gets.chomp.downcase
+          if (answer4_1_1 == 'yes')
+            p 'You leave the city behind and arrive at the Sanctuary 3 days later...'
+            p "A man emerges from the shadows, calling himself 'Negan' and holding a crude weapon saying something about a game of baseball..."
+            p 'Do you play?'
+            answer4_1_1_1 = gets.chomp.downcase
+            if (answer4_1_1_1 == 'no')
+              p "He decides you are playing anyways. He swings the bat at you and everything goes black. You are dead."
+              exit
+            elsif (answer4_1_1_1 == 'yes')
+              p 'Negan charges at you. You only have a split second to make a decision: Hit First, Dodge, Run Away'
+              fight_decision = gets.chomp.downcase
+              if (fight_decision == 'hit first')
+                p 'You trip attempting to throw the first blow. You miss, you are dead'
+                exit
+                elsif (fight_decision == 'dodge')
+                  p 'You dodge the first swing, but his swing speed is no match for you. You are dead'
+                  exit
+                else 
+                  p 'You run to the woods. You lose Negan, but you have also lost Glenn...'
+                  p 'Here lives the Florida Man. Riding an alligator, he takes you away to his lab. There you stay as his pet.'
+                  p 'A year later, you finally see a chance to escape. Do you take it?'
+                
+                    answer3_2 = gets.chomp.downcase 
+                      if (answer3_2.include?('yes'))
+                        p 'He catches you. He eats you for dinner'
+                        exit 
+                      else 
+                        p "Year 2. Florida Man's meth lab blows up. He is covered in flames. Do you put him out or let him burn?"
+                        answer3_3 = gets.chomp.downcase 
+                          if (answer3_3.include?('burn'))
+                            p 'The smoke and the flame attracks a mob of zombies. YOu are surrounded with no escape. You die'
+                            exit 
+                          else 
+                            p 'You put him out. Thankful, he releases you from captivity. He gives you his pet alligator and you ride away into the sunset.'
+                            p 'Congratz on surviving the zombie apocalypse!'
+                            exit 
+                          end
+                          end
+                          end
+                          end
+                        end
+                      end
+                    end
+                  end
 choice4(answer4)
 
 puts 'You reach the outskirts of downtown by sunset. What do you do? Find safe shelter or walk through the night?'
@@ -128,5 +173,8 @@ end
 choice6(answer6)
 
   p 'Better luck next time.....'
+
+end 
+game_start()
 
   
